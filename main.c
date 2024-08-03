952ed5d50a9d0c2af637ccf6262570f0a973ec2b
darcy <acednes@gmail.com>
2024-08-03 12:25:07 +1000

    make better

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <locale.h>
 #include <ncurses.h>
 
 #define MAX_INPUT 20
@@ -36,11 +37,36 @@ void draw_cursor(enum VimMode mode, uint32_t cursor) {
     move(ROW, COL + cursor + offset);
 }
 
+void draw_box_outline(uint32_t x, uint32_t y, uint32_t w, uint32_t h) {
+    move(y - 1, x - 1);
+    addch(ACS_ULCORNER);
+    for (uint32_t i = 0; i < w; ++i) {
+        addch(ACS_HLINE);
+    }
+    addch(ACS_URCORNER);
+
+    for (uint32_t i = 0; i < h; ++i) {
+        move(y + i, x - 1);
+        addch(ACS_VLINE);
+        move(y + i, x + w);
+        addch(ACS_VLINE);
+    }
+
+    move(y + h, x - 1);
+    addch(ACS_LLCORNER);
+    for (uint32_t i = 0; i < w; ++i) {
+        addch(ACS_HLINE);
+    }
+    addch(ACS_LRCORNER);
+}
+
 int main() {
     initscr();
     noecho();
     cbreak();
-    /* keypad(stdscr, TRUE); */
+    setlocale(LC_ALL, "");
+    keypad(stdscr, TRUE);
+    set_escdelay(0);
 
     enum VimMode mode = NORMAL;
 
@@ -48,35 +74,26 @@ int main() {
     uint32_t input_len = 0;
     uint32_t cursor = 0;
 
+    char ch;
+
     while (TRUE) {
-        move(ROW - 1, COL - 1);
-        for (uint32_t i = 0; i < MAX_INPUT + 2; ++i) {
-            printw("-");
-        }
-        move(ROW, COL - 1);
-        printw("|");
+        draw_box_outline(ROW, COL, MAX_INPUT, 1);
+        move(ROW, COL);
         for (uint32_t i = 0; i < MAX_INPUT; ++i) {
             printw("%c", i < input_len ? input[i] : ' ');
         }
-        printw("|");
-        move(ROW + 1, COL - 1);
-        for (uint32_t i = 0; i < MAX_INPUT + 2; ++i) {
-            printw("-");
-        }
 
-        printw("\n");
+        move(ROW + 3, 0);
         printw("mode:   %s\n", mode_name(mode));
         printw("len:    %d\n", input_len);
         printw("cursor: %d\n", cursor);
+        printw("input:  %02x\n", ch);
 
         draw_cursor(mode, cursor);
 
         refresh();
 
-        char ch = getch();
-
-        /* printw("%0x\n", ch); */
-        /* refresh(); */
+        ch = getch();
 
         switch (mode) {
             case NORMAL:
@@ -84,12 +101,14 @@ int main() {
                     case 'q':
                         goto quit;
                     case 'h':
+                    case KEY_LEFT:
                         if (cursor > 0) {
                             --cursor;
                         }
                         break;
                     case 'l':
-                        if (cursor < MAX_INPUT && cursor < input_len) {
+                    case KEY_RIGHT:
+                        if (cursor < MAX_INPUT - 1 && cursor < input_len - 1) {
                             ++cursor;
                         }
                         break;
@@ -110,6 +129,15 @@ int main() {
                         mode = INSERT;
                         cursor = input_len;
                         break;
+                    case '^':
+                    case '_':
+                    case '0':
+                        // TODO: Move to start of input, not 0
+                        cursor = 0;
+                        break;
+                    case '$':
+                        cursor = input_len;
+                        break;
                     case 'D':
                         input_len = cursor;
                         break;
@@ -126,8 +154,21 @@ int main() {
                             --cursor;
                         }
                         break;
+                    case 0x04:
+                        if (cursor > 0) {
+                            --cursor;
+                        }
+                        break;
+                    case 0x05:
+                        if (cursor < MAX_INPUT && cursor < input_len) {
+                            ++cursor;
+                        }
+                        break;
                     case 0x7f:
-                        if (input_len > 0) {
+                        if (cursor > 0 && input_len > 0) {
+                            for (uint32_t i = cursor; i < input_len; ++i) {
+                                input[i - 1] = input[i];
+                            }
                             --input_len;
                             --cursor;
                         }
@@ -135,12 +176,10 @@ int main() {
                     default:
                         if (isprint(ch)) {
                             if (input_len < MAX_INPUT) {
-                                for (uint32_t i = cursor + 1; i < input_len;
-                                     ++i) {
+                                for (uint32_t i = input_len; i >= cursor + 1;
+                                     --i) {
                                     input[i] = input[i - 1];
                                 }
-                                // 012345
-                                // abcxde
                                 input[cursor] = ch;
                                 ++cursor;
                                 ++input_len;
